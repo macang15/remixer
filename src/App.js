6673ac3e2cbb4b494402bd42b3d72aa7d3a9c8a7
macang15 <psbin99@naver.com>
2022-06-24 17:13:49 +0900

    style progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
+import logo from './vinyl.png';
+import searchlogo from './search.png';
 import './App.css';
 import React, {useState} from 'react';
 import axios from 'axios';
 import {useEffect} from 'react';
+import styles from './App.module.css';
+
 
 const initialFormData = Object.freeze({
   name: ''
@@ -58,19 +61,42 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href='/api/login'>LOgin</a>
-
-        <input type='text' name='name' onChange={handleChange}/>
-        <button onClick={handleSearch}>SEARCH</button>
-        <audio controls src = {shownData.preview_url}></audio>
-        <div>
-          <p>Found name: {shownData.real_name} </p>
-          <p>artist: {shownData.artist} </p>
+        <h1 className = {styles.title}>
+          MUSIC REMIXER
+        </h1>
+        <body className={styles.box}>
+          <a  className = {styles.login} href='/api/login'>Login</a>
+        </body>
+        
+        <a> 
+         &nbsp;
+        </a>
+        
+        <body className={styles.searchbox}>
+          <input 
+          placeholder = "please enter your favorite song"
+          style={ {border:"none",outline:"none",width: "850px",
+          height: "50px",color: "white",backgroundColor: "transparent", fontSize: 30, fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"}}
+          type='text' name='name' onChange={handleChange}/>
+          <a> 
+          &nbsp;
+          </a>
+          
+          <button className={styles.search} onClick={handleSearch}><img src={searchlogo} width="30px" height="30px"/></button>
+        </body>
+        <a> 
+          &nbsp;
+          </a>
+          
+        <body className={styles.box2}>
+          <p className = {styles.title2}>Is this what you are looking for?</p>
+          <audio controls src = {shownData.preview_url}></audio>
+          <div>
+            <p>Found name: {shownData.real_name} </p>
+            <p>artist: {shownData.artist} </p>
         </div>
         <img src ={shownData.image_url}></img>
+        </body>
       </header>
     </div>
   );
